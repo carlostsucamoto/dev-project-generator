31047a0d4e0af336254f510290e6e97f700198aa
carlostsucamoto <caduchiarelli@hotmail.com>
2026-05-13 16:13:51 -0300

    feat: add app initial setup

---
+++ app.py
@@ -0,0 +1,5 @@
+from database import abrir_conexao
+from queries import sortear, listar_linguagem, listar_bibliotecas, adicionar_ideias
+
+conexao = abrir_conexao("ideias.db")
+print(sortear(conexao, "projeto", "iniciante"))
