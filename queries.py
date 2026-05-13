d571a43eabf5cd5149736b05440c96020ed80f7a
carlostsucamoto <caduchiarelli@hotmail.com>
2026-05-13 15:40:07 -0300

    feat: add draw idea query

--- queries.py
+++ queries.py
@@ -1,1 +1,12 @@
 from database import abrir_conexao
+import random
+
+
+def sortear(conexao, categoria, dificuldade):
+    cursor = conexao.cursor()
+    cursor.execute("SELECT * FROM ideias WHERE categoria = ? AND dificuldade = ?", (categoria, dificuldade))
+    resultados = cursor.fetchall()
+    sorteio = random.choice(resultados)
+    return sorteio
+if __name__ == "__main__":
+    sortear()
