6dd69e4093bf96a641792fdfc704564ff2e2ccbd
carlostsucamoto <caduchiarelli@hotmail.com>
2026-05-13 16:00:48 -0300

    feat: add all queries functions

--- queries.py
+++ queries.py
@@ -10,3 +10,21 @@ def sortear(conexao, categoria, dificuldade):
     return sorteio
 if __name__ == "__main__":
     sortear()
+
+def listar_linguagem(conexao):
+    cursor = conexao.cursor()
+    cursor.execute("SELECT * FROM linguagens")
+    resultados = cursor.fetchall()
+    return resultados
+
+def listar_bibliotecas(conexao, id_linguagem):
+    cursor = conexao.cursor()
+    cursor.execute("SELECT * FROM bibliotecas WHERE id_linguagem_id = ?", (id_linguagem,))
+    resultados = cursor.fetchall()
+    return resultados
+
+def adicionar_ideias(conexao, nome_ideias, descricao, categoria, dificuldade):
+    cursor = conexao.cursor()
+    cursor.execute("INSERT INTO ideias (nome_ideias, descricao, categoria, dificuldade) VALUES (?, ?, ?, ?)", (nome_ideias, descricao, categoria, dificuldade))
+    resultados = conexao.commit()
+    return resultados
